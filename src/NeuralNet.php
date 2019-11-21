fc78840fd18abf96a59bbaf73ec499f5073f2c92
alex-wdmg <alex.vyshnyvetskyy@gmail.com>
2019-11-21 23:42:35 +0200

    Initial commit

---
+++ src/NeuralNet.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace wdmg\ai;
+
+/**
+ * Multi-layer Neural Network
+ *
+ * @category        Library
+ * @version         0.0.1
+ * @author          Alexsander Vyshnyvetskyy <alex.vyshnyvetskyy@gmail.com>
+ * @link            https://github.com/wdmg/neuralnet
+ * @copyright       Copyright (c) 2019 W.D.M.Group, Ukraine
+ * @license         https://opensource.org/licenses/MIT Massachusetts Institute of Technology (MIT) License
+ *
+ */
+
+/**
+ * Neural Network definition class
+ */
+class NeuralNet
+{
+
+    public function __construct()
+    {
+
+    }
+}
